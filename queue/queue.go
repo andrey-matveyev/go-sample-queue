05cfb0e0664a7dca0765d6cd7aa25fca0b26c9a4
Andrey Matveyev <andrey.v.matveyev@gmail.com>
2025-06-06 20:27:22 +0300

    Added package "queue"

---
+++ queue/queue.go
@@ -0,0 +1,42 @@
+package queue
+
+import (
+	"container/list"
+	"sync"
+)
+
+type queue struct {
+	mtx        sync.Mutex
+	innerChan  chan struct{}
+	queueTasks *list.List
+}
+
+func newQueue() *queue {
+	item := queue{}
+	item.innerChan = make(chan struct{}, 1)
+	item.queueTasks = list.New()
+	return &item
+}
+
+func (item *queue) push(task *Task) {
+	item.mtx.Lock()
+	item.queueTasks.PushBack(task)
+	item.mtx.Unlock()
+}
+
+func (item *queue) pop() *Task {
+	item.mtx.Lock()
+	defer item.mtx.Unlock()
+
+	if item.queueTasks.Len() == 0 {
+		return nil
+	}
+	elem := item.queueTasks.Front()
+	item.queueTasks.Remove(elem)
+	return elem.Value.(*Task)
+}
+
+type Task struct {
+	ID   int
+	Data string
+}
